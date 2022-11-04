3e47bbe0f1bc497dc26a252664d83b3df27bc2e5
lorenzo <lollo.giarella@gmail.com>
2022-11-04 22:06:19 +0100

    First commit, project setup

--- Bit4All/Bit4All/settings.py
+++ Bit4All/Bit4All/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'BitEx'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,8 @@ WSGI_APPLICATION = 'Bit4All.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'BitEx',
     }
 }
 
