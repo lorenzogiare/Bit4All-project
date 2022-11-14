3a0e6862b7d0ef33c9a0e1c20b3813fe0ebcdee6
lorenzo <lollo.giarella@gmail.com>
2022-11-14 22:13:50 +0100

    ok now the ui works

--- Bit4All/BitEx/main.py
+++ Bit4All/BitEx/main.py
@@ -1,11 +1,27 @@
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QFont
+from PyQt5 import uic
+import sys
+
+class Ui(QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+
+        #Load Ui file
+        uic.loadUi('BitEx_GUI.ui', self)
+
+        #Show the App
+        self.show()
 
 
 def main():
-    app = QApplication([])
-    window = QWidget()
-    window.show()
+    
+    #Inizializing the app
+    app = QApplication(sys.argv)
+    UIWindow = Ui()
     app.exec_()
+    
+
 
 
 if __name__ =='__main__':
---
+++ Bit4All/BitEx/views.py
@@ -0,0 +1,18 @@
+
+
+def registration(request):
+    #form da riempire
+    #creazione user e profile
+    #assegnazione dei bitcoin 1-10
+    pass
+
+def login(request):
+    #form da riempire
+    #login dell'utente
+    pass
+
+def make_order(request):
+    #form da riempire
+    #check se si ha abbastanza equity
+    #creazione dell'oggetto ordine 
+    pass
